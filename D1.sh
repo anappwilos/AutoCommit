6176b30efbf9c66ce34107b897ba3d3d570a1509
anappwilos <anappwilos@gmail.com>
2018-04-25 09:26:11 +0200

    Eliminate ??

--- D1.sh
+++ D1.sh
@@ -1,19 +1,15 @@
 #!/bin/sh/ 
-## Part-NameFolder
 echo "Enter the name folder (Ex: MyFolder) :" 
 read NFOLD					
 echo "Enter the number repeat for your folders : (Ex: 10)" 
 read NuF		
 echo "Enter the number by folder";
 read nF
-mkdir $NFOLD 
 for ((i=$nF;i<=$NuF;i++));
-	do
-		mkdir $NFOLD$i
-		cd $NFOLD$i
-		touch Readme.md
-		git add ../$NFOLD$i
-		git commit -m "AC-N$i : CR 'AC$i/'"
-		cd ..
+do
+	rm -rf $NFOLD$i
+	git add $NFOLD$i
+	git commit -m "AC-N$i : Del 'AC$i/'"
+		
 done
 
