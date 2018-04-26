f2d34ee87e2ae10a9f6ebfb82fe2b86328ccb41a
anappwilos <anappwilos@gmail.com>
2018-04-26 17:17:59 +0200

    Act. D1.sh

--- D1.sh
+++ D1.sh
@@ -7,15 +7,14 @@ echo "Enter the number by folder";
 read nF
 for ((i=$nF;i<=$NuF;i++));
 do
-	mkdir $NFOLD$i
-	cd $NFOLD$i
-	touch R
-	git add ../$NFOLD$i
-	git commit -m "AC-N$i : CR 'AC$i/'"
-	cd ..
-	rm -rf $NFOLD$i
-	git add $NFOLD$i
-	git commit -m "AC-N$i : Del 'AC$i/'"
-		
+mkdir $NFOLD$i
+cd $NFOLD$i
+touch R
+git add ../$NFOLD$i
+git commit -m "A-N$i : CR 'A$i/'"
+cd ..
+rm -rf $NFOLD$i
+git add $NFOLD$i
+git commit -m "A-N$i : Del 'A$i/'"	
 done
 
